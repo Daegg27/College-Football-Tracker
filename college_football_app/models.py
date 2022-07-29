c30ae48da44749bea3b4e2c9eba816daa8712209
Dalton Eggleston <egglestondalton@yahoo.com>
2022-07-29 09:35:53 -0700

    skeleton

--- college_football_app/models.py
+++ college_football_app/models.py
@@ -1,4 +1,6 @@
+from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class AppUser(AbstractUser):
@@ -7,6 +9,18 @@ class AppUser(AbstractUser):
         max_length=255,
         unique=True,
     )
+    favorite_team = models.CharField(max_length=100, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = [] # Email & Password are required by default.
+
+class ClassicGame(models.Model):
+
+    user = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    stadium_name = models.CharField(max_length=100, null=True)
+    weather_at_kickoff = models.CharField(max_length=50, null=True)
+    home_team = models.CharField(max_length=100)
+    home_team_score = models.IntegerField(max_length=3)
+    away_team = models.CharField(max_length=100)
+    away_team_score = models.IntegerField(max_length=3)
+    winning_team = models.CharField(max_length=100)
